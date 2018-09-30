a95c13cf23b1372a8a82ca540d1af729498ec366
David Gil <dgilperez@gmail.com>
2018-09-30 19:59:08 +0200

    :recycle: adds a SVG analog clock to the scene!

--- examples/05-time.elm
+++ examples/05-time.elm
@@ -2,7 +2,10 @@ module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
 
 import Browser
 import Html exposing (..)
+import Html.Attributes exposing (style)
 import Html.Events exposing (onClick)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 import Task
 import Time
 
@@ -94,20 +97,182 @@ pauseButtonText model =
         "Pause"
 
 
+type alias TimeRecord =
+    { hour : Int
+    , minute : Int
+    , second : Int
+    }
+
+
+timeToRecord : Model -> TimeRecord
+timeToRecord model =
+    { hour = Time.toHour model.zone model.time
+    , minute = Time.toMinute model.zone model.time
+    , second = Time.toSecond model.zone model.time
+    }
+
+
+type alias Point =
+    ( Int, Int )
+
+
+type alias PointString =
+    ( String, String )
+
+
+getCenter : Int -> Int -> Point
+getCenter radius offset =
+    ( radius + offset, radius + offset )
+
+
+getPointString : Point -> PointString
+getPointString point =
+    ( point |> Tuple.first |> String.fromInt, point |> Tuple.second |> String.fromInt )
+
+
+clockHandlePoint : Int -> Int -> Int -> Point -> Int -> Point
+clockHandlePoint minuteHourOrSecond parts radius center padding =
+    let
+        minuteHourOrSecondFloat =
+            minuteHourOrSecond |> toFloat
+
+        radiusFloat =
+            radius - padding |> toFloat
+
+        x =
+            radiusFloat
+                * cos (pi / -2 + (2 * minuteHourOrSecondFloat * pi) / toFloat parts)
+                + (center |> Tuple.first |> toFloat)
+                - (padding |> toFloat)
+
+        y =
+            radiusFloat
+                * sin (pi / -2 + (2 * minuteHourOrSecondFloat * pi) / toFloat parts)
+                + (center |> Tuple.second |> toFloat)
+                - (padding |> toFloat)
+    in
+    ( x |> round, y |> round )
+
+
+clockHandle : Int -> Int -> Int -> String -> String -> Int -> Svg Msg
+clockHandle timeValue parts radius color width padding =
+    let
+        center =
+            getCenter radius padding
+
+        point =
+            clockHandlePoint (timeValue |> modBy parts) parts radius center padding
+                |> getPointString
+
+        radiusValue =
+            radius |> String.fromInt
+    in
+    line
+        [ x1 (point |> Tuple.first)
+        , y1 (point |> Tuple.second)
+        , x2 radiusValue
+        , y2 radiusValue
+        , stroke color
+        , strokeWidth width
+        ]
+        []
+
+
+clockHourHandle : TimeRecord -> Int -> Int -> Svg Msg
+clockHourHandle time radius padding =
+    clockHandle time.hour 12 radius "orange" "8" padding
+
+
+clockMinuteHandle : TimeRecord -> Int -> Int -> Svg Msg
+clockMinuteHandle time radius padding =
+    clockHandle time.minute 60 radius "black" "2" padding
+
+
+clockSecondHandle : TimeRecord -> Int -> Int -> Svg Msg
+clockSecondHandle time radius padding =
+    clockHandle time.second 60 radius "gray" "1" padding
+
+
+clockHourMark : Int -> Int -> Int -> Svg Msg
+clockHourMark radius padding hour =
+    let
+        center =
+            getCenter radius padding
+
+        position =
+            clockHandlePoint (hour |> modBy 12) 12 radius center padding
+                |> getPointString
+    in
+    circle
+        [ cx (position |> Tuple.first)
+        , cy (position |> Tuple.second)
+        , r "5"
+        , fill "black"
+        ]
+        []
+
+
+clockHourMarks : Int -> Int -> List (Svg Msg)
+clockHourMarks radius padding =
+    List.map (clockHourMark radius padding) (List.range 0 11)
+
+
+clockSphereBase : Int -> Int -> Svg Msg
+clockSphereBase radius padding =
+    let
+        radiusValue =
+            radius |> String.fromInt
+    in
+    circle
+        [ cx radiusValue
+        , cy radiusValue
+        , r (radius - padding |> String.fromInt)
+        , fill "lightgray"
+        ]
+        []
+
+
+clockSphere : Int -> Int -> List (Svg Msg)
+clockSphere radius padding =
+    [ clockSphereBase radius padding ]
+        ++ clockHourMarks radius padding
+
+
+clockSvg : TimeRecord -> Int -> Int -> Svg Msg
+clockSvg time radius padding =
+    let
+        boundingBox =
+            (radius * 2) + padding |> String.fromInt
+    in
+    svg
+        [ width boundingBox
+        , height boundingBox
+        , viewBox ("0 0 " ++ boundingBox ++ " " ++ boundingBox)
+        ]
+        (List.append
+            (clockSphere radius padding)
+            [ clockHourHandle time radius padding
+            , clockMinuteHandle time radius padding
+            , clockSecondHandle time radius padding
+            ]
+        )
+
+
 view : Model -> Html Msg
 view model =
     let
-        hour =
-            String.fromInt (Time.toHour model.zone model.time)
+        timeRecord =
+            timeToRecord model
 
-        minute =
-            String.fromInt (Time.toMinute model.zone model.time)
-
-        second =
-            String.fromInt (Time.toSecond model.zone model.time)
+        timeTitle =
+            (timeRecord.hour |> String.fromInt) ++ ":" ++ (timeRecord.minute |> String.fromInt) ++ ":" ++ (timeRecord.second |> String.fromInt)
     in
     div []
-        [ h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+        [ h1
+            [ Html.Attributes.style "color" "red"
+            ]
+            [ Html.text timeTitle ]
+        , clockSvg timeRecord 100 25
         , button [ onClick TooglePause ]
             [ Html.text (model |> pauseButtonText) ]
         ]
