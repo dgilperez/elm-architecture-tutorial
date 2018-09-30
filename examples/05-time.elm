571f964f19afacb686c06d2c76fbfab118cf3d6a
David Gil <dgilperez@gmail.com>
2018-09-30 17:45:48 +0200

    :recycle: adds a pause / unpause button

--- examples/05-time.elm
+++ examples/05-time.elm
@@ -1,5 +1,8 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+
 import Browser
 import Html exposing (..)
+import Html.Events exposing (onClick)
 import Task
 import Time
 
@@ -9,12 +12,12 @@ import Time
 
 
 main =
-  Browser.element
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    }
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
 
 
 
@@ -22,16 +25,17 @@ main =
 
 
 type alias Model =
-  { zone : Time.Zone
-  , time : Time.Posix
-  }
+    { zone : Time.Zone
+    , time : Time.Posix
+    , paused : Bool
+    }
 
 
-init : () -> (Model, Cmd Msg)
+init : () -> ( Model, Cmd Msg )
 init _ =
-  ( Model Time.utc (Time.millisToPosix 0)
-  , Task.perform AdjustTimeZone Time.here
-  )
+    ( Model Time.utc (Time.millisToPosix 0) False
+    , Task.perform AdjustTimeZone Time.here
+    )
 
 
 
@@ -39,23 +43,28 @@ init _ =
 
 
 type Msg
-  = Tick Time.Posix
-  | AdjustTimeZone Time.Zone
+    = Tick Time.Posix
+    | AdjustTimeZone Time.Zone
+    | TooglePause
 
 
-
-update : Msg -> Model -> (Model, Cmd Msg)
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    Tick newTime ->
-      ( { model | time = newTime }
-      , Cmd.none
-      )
+    case msg of
+        Tick newTime ->
+            ( { model | time = newTime }
+            , Cmd.none
+            )
 
-    AdjustTimeZone newZone ->
-      ( { model | zone = newZone }
-      , Cmd.none
-      )
+        AdjustTimeZone newZone ->
+            ( { model | zone = newZone }
+            , Cmd.none
+            )
+
+        TooglePause ->
+            ( { model | paused = not model.paused }
+            , Cmd.none
+            )
 
 
 
@@ -64,18 +73,41 @@ update msg model =
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every 1000 Tick
+    case model.paused of
+        True ->
+            Sub.none
+
+        False ->
+            Time.every 1000 Tick
 
 
 
 -- VIEW
 
 
+pauseButtonText : Model -> String
+pauseButtonText model =
+    if model.paused then
+        "Restart"
+
+    else
+        "Pause"
+
+
 view : Model -> Html Msg
 view model =
-  let
-    hour   = String.fromInt (Time.toHour   model.zone model.time)
-    minute = String.fromInt (Time.toMinute model.zone model.time)
-    second = String.fromInt (Time.toSecond model.zone model.time)
-  in
-  h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+    let
+        hour =
+            String.fromInt (Time.toHour model.zone model.time)
+
+        minute =
+            String.fromInt (Time.toMinute model.zone model.time)
+
+        second =
+            String.fromInt (Time.toSecond model.zone model.time)
+    in
+    div []
+        [ h1 [] [ text (hour ++ ":" ++ minute ++ ":" ++ second) ]
+        , button [ onClick TooglePause ]
+            [ Html.text (model |> pauseButtonText) ]
+        ]
